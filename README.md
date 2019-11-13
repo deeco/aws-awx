49ec9f87ddf62d5507de1f36948168ef01a24d24
dgowran <dgowran@informatica.com>
2019-11-13 23:18:38 +0000

    intial commit and setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# aws-awx
-aws-awx
+# AWS-Terraform
---
+++ ec2.tf
@@ -0,0 +1,55 @@
+resource "aws_security_group" "awx" {
+  name = "awx"
+  description = "security group for awx"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["92.51.249.9/32"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["92.51.249.9/32"]
+  }
+
+  vpc_id = "${aws_vpc.awx.id}"
+}
+
+resource "aws_instance" "awx1" {
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "terraform_ec2_key"
+  security_groups = ["${aws_security_group.awx.id}"]
+  subnet_id = "${aws_subnet.subnet3.id}"
+  associate_public_ip_address = true
+
+  provisioner "file" {
+    source      = "deploy/"
+    destination = "/tmp/deploy/"
+  }
+
+  tags = {
+      Name = "AWX-APP"
+    }
+}
+
+resource "aws_instance" "awx2" {
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "terraform_ec2_key"
+  security_groups = ["${aws_security_group.awx.id}"]
+  subnet_id = "${aws_subnet.subnet6.id}"
+  associate_public_ip_address = false
+  tags = {
+      Name = "AWX-DB"
+    }
+}
+
+resource "aws_key_pair" "terraform_ec2_key" {
+  key_name = "terraform_ec2_key"
+  public_key = "${file("terraform_ec2_key.pub")}"
+}
