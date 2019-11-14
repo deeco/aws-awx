a5c69d538062b9e3e162547bef1b455381216574
dgowran <dgowran@informatica.com>
2019-11-14 02:19:57 +0000

    docker compose and aws

--- deploy/install_awx.sh
+++ deploy/install_awx.sh
@@ -13,5 +13,11 @@ sudo apt-get install git -y
 # clone repository
 git clone https://github.com/deeco/aws-awx /tmp/aws-awx
 
-# run playbook
+# install role pre-req
+sudo ansible-galaxy install geerlingguy.docker
+
+# run playbook to install docker
 ansible-playbook --connection=local 127.0.0.1 /tmp/aws-awx/deploy/docker.yml
+
+# run docker compose to install awx
+sudo docker-compose -f /tmp/aws-awx/deploy/awx/docker-compose.yml up -d 
